451bbb7d121df8e89573833ab8409bacbe494d88
Seemanta Torafdar <seemanta11189@gmail.com>
2025-08-01 23:55:26 +0600

    yes

---
+++ Mid/firstlab.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html> 
+    <body>
+        <title> Seemanta</title>
+        <h1 style="text-align: center;"> SEEMANTA</h1>
+        <P style="text-align: center;"> <i>My self Seemanta Torafdar.<br>
+            I am a student of Aiub at the depertment of CSE . <br>I love to traveling and play Badminton .<br>
+            I enjpy to solving problem and exploeing the new idea .<br>
+            I beliving in honest,hardworking and respectful to others </i></P>
+
+
+            <h3 > Hobbies</h3>
+            <ol >
+                <li>Playing Game </li>
+                <li >Playing Game  </li>
+                <li>Traveling  </li>
+                <li> Playing Football and Badminton   </li>
+            
+            </ol>
+            <p style="color:red;" > Red</p>
+             <p style="color:blue;" > Blue </p>
+              <p style="color:mediumaquamarine;" > Mediumquamrine</p>
+              <a href="https://www.w3schools.com">w3school</a>
+              <br>
+               <br>
+                <br>
+              <img src="MV5BNmU3ZDNhN2MtOWEzMi00YTdlLThmNmEtNGYzNGNmNmQyOGY0XkEyXkFqcGc@._V1_.jpg"width="400" height="400">
+
+    </body>
+</html>
